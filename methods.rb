697af95cc51dcb4193131fe20c982ad330348841
chrisdennis12345@gmail.com <chrisdennis@Chriss-iMac.local>
2022-10-11 16:26:33 -0700

    commit tree 2f20b96c4cb907af37b97a7234507dba5847e833

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+# function myFunction(param) {
+#   console.log("Running myFunction");
+#   return param + 1;
+# }
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+  end
+
+  def halve(num)
+    return nil unless num.class == Integer
+    
+    num / 2
+  end
